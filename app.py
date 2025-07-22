d59cf70dc709a12c94d6d307391d559e5918a8f0
Sania-Sajit <saniasajit05@gmail.com>
2025-07-22 11:12:17 +0000

    added page for each club

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask,render_template,jsonify
-from database import load_clubs_from_db
+from database import load_clubs_from_db,load_club_from_db
 
 app=Flask(__name__)
 
@@ -14,6 +14,15 @@ def hello():
 def list_clubs():
     clubs=load_clubs_from_db()
     return jsonify(clubs)
+
+@app.route("/club/<id>")
+def show_club(id):
+    club=load_club_from_db(id)
+    if not club:
+        return "Not Found",404
+    return render_template('clubpage.html',club=club)
+    
+    
     
 if __name__ == '__main__':
     app.run(host='0.0.0.0',debug=True)
--- database.py
+++ database.py
@@ -14,6 +14,17 @@ def load_clubs_from_db():
           clubs.append(dict(row._mapping))
       return clubs
 
+def load_club_from_db(id):
+  with engine.connect() as conn:
+      result = conn.execute(text("SELECT * FROM clubs WHERE id = :val"),{"val":id})
+
+      rows=result.all()
+
+      if len(rows)==0:
+        return None
+      else:
+        return dict(rows[0]._mapping)
+
 
 
 
