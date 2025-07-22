7f54df5a06f28b0ad882722e87eb30013fbedb20
Sania-Sajit <saniasajit05@gmail.com>
2025-07-22 05:59:02 +0000

    added db and made webpage dynamic

--- app.py
+++ app.py
@@ -1,36 +1,19 @@
 from flask import Flask,render_template,jsonify
+from database import load_clubs_from_db
 
 app=Flask(__name__)
 
-Clubs = [{'id':1,
-          'name':"Robotics Club",
-          'domain':"Technical",
-          'Description':"Designing the future, one robot at a time.",
-          "head":"Amy",
-          'logo':"robo.jpg"},
-         
-         {'id':2,
-          'name':"Music Club",
-          'domain':"Cultural",
-          'Description':"Where melodies come alive and voices unite.",
-          "head":"Brian",
-          'logo':"music.jpg"},
 
-         {'id':3,
-          'name':"Reading Club",
-          'domain':"Literary",
-          'Description':"Journey through stories, one page at a time.",
-          "head":"Charles",
-          'logo':"read.jpg"}
-         
-        ]
+
 @app.route('/')
 def hello():
-    return render_template('home.html',clubs=Clubs)
+    clubs=load_clubs_from_db()
+    return render_template('home.html',clubs=clubs)
 
 @app.route('/api/clubs')
 def list_clubs():
-    return jsonify(Clubs)
+    clubs=load_clubs_from_db()
+    return jsonify(clubs)
     
 if __name__ == '__main__':
     app.run(host='0.0.0.0',debug=True)
--- database.py
+++ database.py
@@ -1,1 +1,21 @@
-import sqlalchemy
+from sqlalchemy import create_engine,text
+import os
+
+db_connection_string = os.environ['DB_CONNECTION_STRING']
+
+engine=create_engine(db_connection_string )
+
+def load_clubs_from_db():
+  with engine.connect() as conn:
+      result = conn.execute(text("SELECT * FROM clubs"))
+
+      clubs = []
+      for row in result.all():
+          clubs.append(dict(row._mapping))
+      return clubs
+
+
+
+
+
+
