217eab1211abb61de8fdd622e870205686f4a7e5
Sania-Sajit <saniasajit05@gmail.com>
2025-07-22 20:05:34 +0000

    made form and connected it to db

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask,render_template,jsonify
-from database import load_clubs_from_db,load_club_from_db
+from flask import Flask,render_template,jsonify,request
+from database import load_clubs_from_db,load_club_from_db,add_application_to_db
 
 app=Flask(__name__)
 
@@ -22,7 +22,13 @@ def show_club(id):
         return "Not Found",404
     return render_template('clubpage.html',club=club)
     
-    
+@app.route("/club/<id>/apply", methods=['Post'])
+def apply_to_club(id):
+    club=load_club_from_db(id)
+    data=request.form
+
+    add_application_to_db(id,data)
+    return render_template("submitted.html", application=data,club=club)
     
 if __name__ == '__main__':
     app.run(host='0.0.0.0',debug=True)
--- database.py
+++ database.py
@@ -26,6 +26,20 @@ def load_club_from_db(id):
         return dict(rows[0]._mapping)
 
 
+def add_application_to_db(club_id, data):
+  with engine.begin() as conn:
+    query = text("""
+      INSERT INTO applications(club_id, name, stud_id, phone, email, why_join) 
+      VALUES (:club_id, :name, :stud_id, :phone, :email, :why_join)
+    """)
+    conn.execute(query, {
+      "club_id": club_id,
+      "name": data['name'],
+      "stud_id": data['stud-id'],
+      "phone": data['phone'],
+      "email": data['mail'],
+      "why_join": data['why-join']
+    })
 
 
 
